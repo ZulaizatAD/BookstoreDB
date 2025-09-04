893487bf58dbb1d598e9d1d160035897bf760750
Zulaizat <zulaizatad@gmail.com>
2025-09-04 10:14:42 +0800

    commit

--- backend/db/db_connection.py
+++ backend/db/db_connection.py
@@ -7,19 +7,20 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 print("DB Username:", os.getenv("DB_USERNAME"))
 
-# Database URL
+# Database URL components
 DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
-# Database URL
-DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Check for required envs
 required_envs = [DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME]
 if not all(required_envs):
-    raise ValueError("Some database environment variables are missing in .env file!")
+    raise ValueError("Some database environment variables are missing in .env file or environment!")
+
+# Construct Database URL
+DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Set up SQLAlchemy Engine and Base
 engine = create_engine(DATABASE_URL)
@@ -29,7 +30,7 @@ Base = declarative_base()
 # Create tables automatically when the module is imported
 def init_table():
     Base.metadata.create_all(engine)
-    
+
 # Function to get a database session
 def get_db():
     db = SessionLocal()
